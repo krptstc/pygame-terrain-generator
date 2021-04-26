f54c8ca3188d21fda60d36e9a914f265118beeac
krptstc <krptstc@seznam.cz>
2021-04-26 20:44:56 +0200

    Adding Pygame

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
- 
+import pygame
+pygame.init()
+
+SCREEN_WIDTH  = 640
+SCREEN_HEIGHT = 480
+TILE_SIZE     = 16
+
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Terrain Generator')
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
